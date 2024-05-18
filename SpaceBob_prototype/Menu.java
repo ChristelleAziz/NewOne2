3fd4e1631498c72e7fa824ab67c76d1af3a65427
%username% <%email%>
2024-05-18 02:13:52 -0400

    Started Menu, added menu art and classes

---
+++ SpaceBob_prototype/Menu.java
@@ -0,0 +1,11 @@
+import greenfoot.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Menu extends Actor {
+
+    public Menu() {    
+    
+    }
+}
+
---
+++ SpaceBob_prototype/Menu_Screen.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Menu_Screen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu_Screen extends World
+{
+
+    /**
+     * Constructor for objects of class Menu_Screen.
+     * 
+     */
+    public Menu_Screen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
