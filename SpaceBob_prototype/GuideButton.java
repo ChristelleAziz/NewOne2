a1812f130a6deeaaf0e94e036d18a29794659ee1
%username% <markololo2468@gmail.com>
2024-05-23 19:10:21 -0400

    Menu Screen

---
+++ SpaceBob_prototype/GuideButton.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GuideButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GuideButton extends Actor
+{
+    /**
+     * Act - do whatever the GuideButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- SpaceBob_prototype/Menu.java
+++ SpaceBob_prototype/Menu.java
@@ -17,5 +17,22 @@ public class Menu extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        WholeMenu wholeMenu = new WholeMenu();
+        addObject(wholeMenu,306,188);
+        ExitButton exitButton = new ExitButton();
+        addObject(exitButton,308,312);
+        GuideButton guideButton = new GuideButton();
+        addObject(guideButton,309,255);
+        PlayButton playButton = new PlayButton();
+        addObject(playButton,305,197);
     }
 }
